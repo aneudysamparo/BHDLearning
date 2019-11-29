aa0895c50213cc67d1e1c5576e0ccb8910abb15d
Argenis Villarroel <smitterargenis@hotmail.com>
2019-11-28 21:52:46 -0400

    Correct Learning

---
+++ BHDLearning.Testing/Infrastructure/UnitTestBase.cs
@@ -0,0 +1,30 @@
+﻿using BHDLearning.RestApi.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace BHDLearning.Testing.Infrastructure
+{
+    public abstract class UnitTestBase<T>
+        where T: ApiStartupBase
+    {
+        protected TestServer testServer;
+        protected HttpClient client;
+
+        public UnitTestBase()
+        {
+            testServer = new TestServer(GetHost());
+            client = testServer.CreateClient();
+        }
+
+        protected virtual IWebHostBuilder GetHost()
+        {
+            return new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<T>();
+        }
+    }
+}
